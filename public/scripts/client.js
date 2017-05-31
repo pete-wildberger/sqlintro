bd48eaed16e3a07148af16187d0bbe9f9aeebf41
Peter Wildberger <pete.wildberger@gmail.com>
2017-05-31 16:00:12 -0500

    WORKS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,33 @@
+
+$(onReady);
+
+function onReady(){
 console.log('JS/JQ loaded');
+addImage();
+getImages();
+}
+
+var getImages = function(){
+  $.ajax({
+    type: 'GET',
+    url: '/images',
+    success: function(response ){
+      console.log(response);
+    }
+  });
+};
+
+var addImage = function(){
+  // test get call to server
+  var objectToSend = {
+    thing: 'meow'
+  }; //end objectToSend
+  $.ajax({
+    type: 'POST',
+    url: '/images',
+    data: objectToSend,
+    success: function( response ){
+      console.log( 'back from post call with:', response );
+    } // end success
+  }); // end ajax
+}; // end getImages
--- server.js
+++ server.js
@@ -3,8 +3,19 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var pg = require('pg');
+
 //globals
 var port = 9001;
+var config = {
+  database: 'omegapicts',
+  host: 'localhost',
+  port: 5432,
+  max: 12
+};
+
+var pool = new pg.Pool( config );
+
 //uses
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded( {extended: true}));
@@ -16,4 +27,33 @@ app.listen(port, function(){
 app.get('/', function(req, res){
   console.log('base url hit');
   res.sendFile(path.resolve('views/index.html'));
+});
+
+app.get('/images', function(req, res){
+  console.log('images url hit');
+  pool.connect(function(err, connection, done){
+    if(err){
+      console.log('error connecting to db');
+      done();
+      res.send('poop');
+    }else{
+      console.log('connect to db');
+        var allImages = [];
+
+
+
+        var resultSet = connection.query( 'SELECT * FROM pictable' );
+        resultSet.on('row', function(row){
+          allImages.push(row);
+        });
+        resultSet.on('end', function(){
+          done();
+          res.send(allImages);
+        });
+    }
+  });
+});
+
+app.post('/images', function(req, res){
+  res.send('hey');
 });
